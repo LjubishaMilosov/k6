a347bdaf30a88f7c8b1c1f4d38ad957da5a9b4df
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-03-11 04:36:06 +0800

    stress test

--- load-test.js
+++ load-test.js
@@ -1,27 +1,28 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
 
 export const options = {
-  stages:[
+  stages: [
     {
-      duration: '5m',
-      target: 100
+      duration: "5m",
+      target: 100,
     },
     {
-      duration: '30m',
-      target: 100
+      duration: "30m",
+      target: 100,
     },
     {
-      duration: '5m',
-      target: 0
-    }
-  ]};
+      duration: "5m",
+      target: 0,
+    },
+  ],
+};
 
 export default function () {
-  http.get('https://test.k6.io');
+  http.get("https://test.k6.io");
   sleep(1);
-  http.get('https://test.k6.io/contact.php');
+  http.get("https://test.k6.io/contact.php");
   sleep(2);
-  http.get('https://test.k6.io/news.php');
+  http.get("https://test.k6.io/news.php");
   sleep(2);
-}   
+}
