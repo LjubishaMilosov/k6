dd15825016a6e1ff80b662da3b4102dac567b6e3
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-03-18 01:16:32 +0800

    post requests

---
+++ http-post.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export default function () {
+
+    const body = JSON.stringify({
+        username: "test_" + Date.now(),
+        password: "test"
+    });
+
+    const params = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+  const res = http.get("https://test-api.k6.io/user/register", body, params);
+
+}
