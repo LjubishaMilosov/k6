41c5e761978262102298f264ff2ff2a359133b47
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-03-19 05:14:46 +0800

    create & retrieve a resource

--- http-post.js
+++ http-post.js
@@ -1,54 +1,76 @@
-
-
-import http from 'k6/http';
-import { check } from 'k6';
+import http from "k6/http";
+import { check } from "k6";
 
 export default function () {
+  const credentials = {
+    username: "test_" + Date.now(),
+    password: "secret_" + Date.now(),
+  };
 
-    const credentials = {
-        username: 'test_' + Date.now(),
-        password: 'secret_' + Date.now()
-    }
-
-
-    http.post('https://test-api.k6.io/user/register/',
+  http.post(
+    "https://test-api.k6.io/user/register/",
     JSON.stringify(credentials),
     {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
-    );
+  );
 
-    let res = http.post(
-        'https://test-api.k6.io/auth/token/login/',
-        JSON.stringify(
-            {
-                username: credentials.username,
-                password: credentials.password
-            }
-        ),
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-    );
+  let res = http.post(
+    "https://test-api.k6.io/auth/token/login/",
+    JSON.stringify({
+      username: credentials.username,
+      password: credentials.password,
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-    const accessToken = res.json().access;
-    console.log(accessToken);
+  const accessToken = res.json().access;
+  console.log(accessToken);
 
+  http.get("https://test-api.k6.io/my/crocodiles", {
+    headers: {
+      Authorization: "Bearer " + accessToken,
+      "Content-Type": "application/json",
+    },
+  });
 
-    http.get(
-        'https://test-api.k6.io/my/crocodiles',
-        {headers: 
-            {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json'
-            }
-        }
-    );
+  res = http.post(
+    "https://test-api.k6.io/my/crocodiles/",
+    JSON.stringify({
+      name: "Random croc",
+      sex: "M",
+      date_of_birth: "1900-10-28",
+    }),
+    {
+      headers: {
+        Authorization: "Bearer " + accessToken,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const newCrocodileId = res.json().id;
+
+  res = http.get(`https://test-api.k6.io/my/crocodiles/${newCrocodileId}/`, 
+  {
+    headers: 
+    {
+      Authorization: "Bearer " + accessToken,
+    },
+  });
+  check(res, {
+    'status is 20': (r) => r.status === 200,
+    'crocoile id':(r) => r.json().id === newCrocodileId,
+    'crocoile name':(r) => r.json().name === "Random croc"
+  });
+  
+
 }
 
-
-//   k6 run --http-debug="full" http-post.js
+// //   k6 run --http-debug="full" http-post.js
